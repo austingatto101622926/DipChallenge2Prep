72b514361124c4d7d439852f4a6a553205ecb0ac
Age <STUDENT@DESKTOP-G5ACJVG>
2018-06-29 08:36:27 +1000

    generic dataAccess

---
+++ DipChallenge2Prep_DataHandler/DataAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DipChallenge2Prep_DataHandler
+{
+    public class DataAccess
+    {
+        public ControllerHandler<Type> Handler;
+
+        public DataAccess(string baseAddress)
+        {
+            Handler = new ControllerHandler<Type>(baseAddress, "controlPath"); 
+        }
+    }
+}
